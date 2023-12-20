4444f68681e3bd93ca15074a8180ff22df8e3087
mahan owji <mahanowji1380@gmail.com>
2023-12-20 12:27:25 +0330

    Applied the migration and the update-database command, looks just perfect and excpected as it was in the DB

---
+++ src/Infrastructure/Persistants/Migrations/20231220085435_init.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Persistants.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Mahan_Owji");
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                schema: "Mahan_Owji",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Field = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    NumberOfEmployees = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                schema: "Mahan_Owji",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "Mahan_Owji",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GenderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    IdentityCode = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: true),
+                    Nationality = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Genders_GenderId",
+                        column: x => x.GenderId,
+                        principalSchema: "Mahan_Owji",
+                        principalTable: "Genders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                schema: "Mahan_Owji",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cars_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Mahan_Owji",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCompanyJunks",
+                schema: "Mahan_Owji",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCompanyJunks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserCompanyJunks_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalSchema: "Mahan_Owji",
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCompanyJunks_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Mahan_Owji",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "Mahan_Owji",
+                table: "Genders",
+                columns: new[] { "Id", "IsDeleted", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("2455d6a4-5236-4acc-a031-17835b8420f7"), false, "Male" },
+                    { new Guid("907e5906-d524-4a27-b1d9-4b558667a0f1"), false, "Female" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_UserId",
+                schema: "Mahan_Owji",
+                table: "Cars",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCompanyJunks_CompanyId",
+                schema: "Mahan_Owji",
+                table: "UserCompanyJunks",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCompanyJunks_UserId",
+                schema: "Mahan_Owji",
+                table: "UserCompanyJunks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_GenderId",
+                schema: "Mahan_Owji",
+                table: "Users",
+                column: "GenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IdentityCode",
+                schema: "Mahan_Owji",
+                table: "Users",
+                column: "IdentityCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars",
+                schema: "Mahan_Owji");
+
+            migrationBuilder.DropTable(
+                name: "UserCompanyJunks",
+                schema: "Mahan_Owji");
+
+            migrationBuilder.DropTable(
+                name: "Companies",
+                schema: "Mahan_Owji");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "Mahan_Owji");
+
+            migrationBuilder.DropTable(
+                name: "Genders",
+                schema: "Mahan_Owji");
+        }
+    }
+}
