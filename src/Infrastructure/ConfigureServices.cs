85baca22a4c100e6ab8d8d9d4bb812c1dd2bb290
mahan owji <mahanowji1380@gmail.com>
2023-12-20 12:20:25 +0330

    built the connectionstring and configured the infrastructure service , ready for migration and update-database

---
+++ src/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Infrastructure.Persistants;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(configuration.GetConnectionString("UserDataHubConnectionString")));
+            return services;
+        }
+    }
+}
---
+++ src/Infrastructure/Persistants/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿
+using Domain;
+using System.Reflection;
+
+namespace Infrastructure.Persistants
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<GenderEntity> GenderEntites { get; set; }
+        public DbSet<UserEntity> UserEntities { get; set; }
+        public DbSet<CarEntity> CarEntities { get; set; }
+        public DbSet<CompanyEntity> CompanyEntites { get; set; }
+        public DbSet<UserCompanyEntityJunk> UserCompanyJunkEntites { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.HasDefaultSchema(DomainSchema.schema);
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+    
+}
