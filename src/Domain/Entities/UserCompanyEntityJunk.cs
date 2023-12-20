8e8afd31afec6fd1abc049b1d62a94e03e6b4795
mahan owji <mahanowji1380@gmail.com>
2023-12-20 08:29:11 +0330

    added the entites , firs real commit

---
+++ src/Domain/Entities/UserCompanyEntityJunk.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Commons;
+
+
+namespace Domain.Entities
+{
+    public class UserCompanyEntityJunk:BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public Guid CompanyId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+
+
+
+        public UserEntity? User { get; set; }
+        public CompanyEntity? Company { get; set; }  
+    }
+}
---
+++ src/Domain/Entities/UserEntity.cs
@@ -0,0 +1,35 @@
+﻿
+using Domain.Commons;
+
+namespace Domain.Entities
+{
+    public class UserEntity:BaseEntity
+    {
+
+        public UserEntity()
+        {
+            Cars = new List<CarEntity>();
+            UserCompanyjunks = new List<UserCompanyEntityJunk>();
+        }
+        public Guid GenderId { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string IdentityCode { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public string? Nationality { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+
+        
+        public required GenderEntity Gender { get; set; }
+
+        public ICollection<CarEntity> Cars { get; set; }
+
+        public ICollection<UserCompanyEntityJunk> UserCompanyjunks { get; set;}
+    }
+}
