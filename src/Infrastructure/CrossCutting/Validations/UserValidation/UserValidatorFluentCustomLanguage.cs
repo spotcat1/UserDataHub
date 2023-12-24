2d594630f4863db8ee17d538b9ab56af83b043fe
mahan owji <mahanowji1380@gmail.com>
2023-12-24 21:30:23 +0330

    test for the sln change

--- src/Infrastructure/CrossCutting/Validations/UserValidation/UserValidatorFluentCustomLanguage.cs
+++ src/Infrastructure/CrossCutting/Validations/UserValidation/UserValidatorFluentCustomLanguage.cs
@@ -12,8 +12,8 @@ namespace Infrastructure.CrossCutting.Validations.UserValidation
             AddTranslation("fa", "NotEmptyValidator", "{PropertyName}   الزامی میباشد ");
             AddTranslation("fa", "MaximumLengthValidator", "{PropertyName} بیش از حد مشخص شده است  طول ");
             //AddTranslation("fa", "LengthValidator", "{PropertyName}   الزامی میباشد ");
-            //AddTranslation("fa", "RegularExpressionValidator", "{PropertyName}  باید فقط حاوی حروف باشد ");
-            //AddTranslation("fa", "RegularExpressionValidator", "{PropertyName}   باید فقط حاوی ارقام باشد ");
+            AddTranslation("fa", "RegularExpressionValidator", "{PropertyName}  باید فقط حاوی حروف باشد ");
+            AddTranslation("fa", "RegularExpressionValidator", "{PropertyName}   باید فقط حاوی ارقام باشد ");
 
         }
     }
